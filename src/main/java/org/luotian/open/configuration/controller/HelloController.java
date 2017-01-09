dd53ffdf49843481de446b3b37d3e4fc9adbc8c8
xiezg <xiezg@317hu.com>
2017-01-09 18:56:23 +0800

    springmvc框架测试代码编写

--- src/main/java/org/luotian/open/configuration/controller/HelloController.java
+++ src/main/java/org/luotian/open/configuration/controller/HelloController.java
@@ -1,11 +1,16 @@
 package org.luotian.open.configuration.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.luotian.open.configuration.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-  
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class HelloController {
 
@@ -19,6 +24,15 @@ public class HelloController {
      mv.addObject("message", helloService.test("管理员"));
      mv.setViewName("hello");
      return mv;
+   }
+
+   @ResponseBody
+   @RequestMapping("/world")
+   public String world() {
+       Map<String, String> worldMap = new HashMap<String, String>();
+       worldMap.put("key1","value1");
+       worldMap.put("key2", "value2");
+       return JSON.toJSONString(worldMap);
    }
 
 }
