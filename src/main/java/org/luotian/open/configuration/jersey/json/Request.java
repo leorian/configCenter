242c8c643b5cb9f3e9d3f9d76b9e5753a9a6d7e5
leorain <1573623031@qq.com>
2017-01-10 19:01:39 +0800

    Spring Jersey Json

---
+++ src/main/java/org/luotian/open/configuration/jersey/json/Request.java
@@ -0,0 +1,13 @@
+package org.luotian.open.configuration.jersey.json;
+
+public class Request {
+    private String query;  
+  
+    public String getQuery() {  
+        return query;  
+    }  
+  
+    public void setQuery(String query) {  
+        this.query = query;  
+    }  
+}  
---
+++ src/main/java/org/luotian/open/configuration/jersey/json/Response.java
@@ -0,0 +1,22 @@
+package org.luotian.open.configuration.jersey.json;
+
+public class Response {
+    private int respCode;  
+    private String respDesc;  
+      
+    public int getRespCode() {  
+        return respCode;  
+    }  
+      
+    public void setRespCode(int respCode) {  
+        this.respCode = respCode;  
+    }  
+      
+    public String getRespDesc() {  
+        return respDesc;  
+    }  
+      
+    public void setRespDesc(String respDesc) {  
+        this.respDesc = respDesc;  
+    }  
+}  
