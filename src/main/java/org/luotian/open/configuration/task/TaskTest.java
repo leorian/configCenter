8d7571d05feb0785954717c81fe9cf298851d255
leorain <1573623031@qq.com>
2017-01-14 12:03:44 +0800

    task

---
+++ src/main/java/org/luotian/open/configuration/task/TaskTest.java
@@ -0,0 +1,10 @@
+package org.luotian.open.configuration.task;
+
+/**
+ * Created by xiezhonggui on 17-1-14.
+ */
+public class TaskTest {
+    public void taskExecute() {
+        System.out.println("定时任务!!!");
+    }
+}
